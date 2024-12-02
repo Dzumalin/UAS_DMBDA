41806a03fde3d8de75176661050910b341854179
Dzumalin <dzumalinkertawijaya@gmail.com>
2024-12-02 08:55:17 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# Membaca data
+file_path = "C:\Streamlit\heart.csv"  # Ganti dengan path file Anda
+data = pd.read_csv(file_path)
+
+# Menampilkan teks di aplikasi
+st.title("Analisis Dataset Penyakit Jantung")
+st.write("Ini adalah aplikasi analisis dataset penyakit jantung.")
+
+# Menampilkan dataset
+st.subheader("Data Awal")
+st.write(data.head())
+
+# Membuat distribusi target
+st.subheader("Distribusi Target")
+fig, ax = plt.subplots(figsize=(6, 4))
+sns.countplot(data=data, x='target', palette='Set2', ax=ax)
+st.pyplot(fig)
+
+# Menampilkan heatmap korelasi
+st.subheader("Heatmap Korelasi")
+fig, ax = plt.subplots(figsize=(10, 8))
+correlation_matrix = data.corr()
+sns.heatmap(correlation_matrix, annot=True, fmt=".2f", cmap="coolwarm", linewidths=0.5, ax=ax)
+st.pyplot(fig)
